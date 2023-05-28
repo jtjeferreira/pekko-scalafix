3db270880e3bf5504dba19c903d9e05c5114b6c9
João Ferreira <jtjeferreira@gmail.com>
2023-05-28 21:05:46 +0100

    fix PekkoScalafixSignificantIndentation

--- input/src/main/scala-3/fix/PekkoScalafixSignificantIndentation.scala
+++ input/src/main/scala-3/fix/PekkoScalafixSignificantIndentation.scala
@@ -3,6 +3,14 @@ rule = PekkoScalafix
 */
 package fix
 
+import akka._
+import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorLogging}
+import akka.Done
+
 object PekkoScalafixSignificantIndentation:
-  val a = 1
   // Add code that needs fixing here.
+  val actorSystem = ActorSystem()
+
+  import akka.actor._
+
--- output/src/main/scala-3/fix/PekkoScalafixSignificantIndentation.scala
+++ output/src/main/scala-3/fix/PekkoScalafixSignificantIndentation.scala
@@ -1,5 +1,12 @@
 package fix
 
+import org.apache.pekko._
+import org.apache.pekko.actor.ActorSystem
+import org.apache.pekko.actor.{Actor, ActorLogging}
+import org.apache.pekko.Done
+
 object PekkoScalafixSignificantIndentation:
-  val a = 1
   // Add code that needs fixing here.
+  val actorSystem = ActorSystem()
+
+  import org.apache.pekko.actor._
