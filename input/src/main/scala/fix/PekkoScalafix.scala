80f95dddd74105bace98c615a07a4a979ebaa73d
João Ferreira <jtjeferreira@gmail.com>
2023-05-30 22:13:20 +0100

    add more examples

--- input/src/main/scala/fix/PekkoScalafix.scala
+++ input/src/main/scala/fix/PekkoScalafix.scala
@@ -8,6 +8,8 @@ import akka.actor.ActorSystem
 import akka.actor.{Actor, ActorLogging}
 import akka.Done
 import akka.AkkaVersion
+import akka.serialization.jackson.AkkaJacksonModule
+import akka.serialization.jackson._
 
 object PekkoScalafix {
   // Add code that needs fixing here.
@@ -17,6 +19,10 @@ object PekkoScalafix {
   val e: AkkaException = ???
   val v: AkkaVersion.type = ???
   val p: dispatch.ForkJoinExecutorConfigurator.AkkaForkJoinPool = ???
+
+  //Classes that need renaming - serialization
+  val jm: AkkaJacksonModule = ???
+  val sjm: AkkaStreamJacksonModule = ???
 
   import akka.actor._
 }
--- output/src/main/scala/fix/PekkoScalafix.scala
+++ output/src/main/scala/fix/PekkoScalafix.scala
@@ -5,6 +5,8 @@ import org.apache.pekko.actor.ActorSystem
 import org.apache.pekko.actor.{Actor, ActorLogging}
 import org.apache.pekko.Done
 import org.apache.pekko.PekkoVersion
+import org.apache.pekko.serialization.jackson.PekkoJacksonModule
+import org.apache.pekko.serialization.jackson._
 
 object PekkoScalafix {
   // Add code that needs fixing here.
@@ -14,6 +16,10 @@ object PekkoScalafix {
   val e: PekkoException = ???
   val v: PekkoVersion.type = ???
   val p: dispatch.ForkJoinExecutorConfigurator.PekkoForkJoinPool = ???
+
+  //Classes that need renaming - serialization
+  val jm: PekkoJacksonModule = ???
+  val sjm: PekkoStreamJacksonModule = ???
 
   import org.apache.pekko.actor._
 }
